763fb54374b1a15ca1213499ed5a719a5158ef72
Brood Bear <broodbear@proton.me>
2024-07-09 11:43:12 -0400

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use clap::Parser;
 use sha2::{Digest, Sha256};
 use std::{
-    fs::{self, metadata},
-    os::unix::fs::MetadataExt,
+    fs::{self},
     io::Result,
 };
-use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
 
 /// Simple program to check file integrity
 #[derive(Parser, Debug)]
@@ -29,33 +28,18 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     let wd_entry = WalkDir::new(args.path)
-    .into_iter()
-    .filter_map(|e| e.ok())
-    .filter_map(check_is_dir);
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !e.path_is_symlink())
+        .filter(|e| e.file_type().is_file())
+        .filter(|e| e.metadata().unwrap().len() < (args.max_file_size * 1024 * 1024));
 
     wd_entry.for_each(|wd_entry| {
-        let md = match metadata(&wd_entry.path()) {
-            Ok(entry) => entry,
-            Err(e) => {
-                println!("Error '{}'", e);
-                return
-            },
-        };
-
-        if args.max_file_size != 0 && md.size() > (args.max_file_size * 1024 * 1024) {
-            if args.verbose {
-                println!(
-                    "Skipping '{}' due to size",
-                    wd_entry.path().to_str().unwrap()
-                );
-            }
-            return;
-        }
-
         tokio::spawn(async move {
             let mut hash = Sha256::new();
 
-            let contents = fs::read(&wd_entry.path()).expect("Should have been able to read the file");
+            let contents =
+                fs::read(&wd_entry.path()).expect("Should have been able to read the file");
             hash.update(contents);
             let result = hash.finalize();
             let hex_result = hex::encode(result);
@@ -67,20 +51,4 @@ async fn main() -> Result<()> {
     });
 
     Ok(())
-}
-
-fn check_is_dir(entry: DirEntry) -> Option<DirEntry> {
-    let md = match metadata(&entry.path()) {
-        Ok(entry) => entry,
-        Err(e) => {
-            println!("Error '{}'", e);
-            return None
-        },
-    };
-
-    if md.is_dir() {
-        return None;
-    }
-
-    Some(entry)
 }
